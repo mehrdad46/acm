2f002cbe846fa08223e501d411e0801ed2041714
Mehran Kholdi <semekh.dev@gmail.com>
2012-07-27 00:52:10 +0430

    added old template

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <set>
+#include <utility>
+#include <string>
+#include <vector>
+#include <valarray>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <sstream>
+#include <complex>
+#include <iomanip>
+#include <cassert>
+
+#define show(x) cerr << "# " << #x << " = " << x << endl
+
+#define FOR(i, n) for(int i = 0; i < (n); i++)
+#define SZ(x) ((int)x.size())
+#define PB push_back
+#define F first
+#define S second
+#define X real()
+#define Y imag()
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef complex<double> point;
+
+const double eps = 1e-9;	//FIXME
+
+int main()
+{
+	return 0;
+}
